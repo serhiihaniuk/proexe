944123c99c3f5f16ab5cf747c25c53b2fba47cd4
serhiihaniuk <serhii.haniuk@gmail.com>
2023-03-25 00:21:54 +0100

    fix page name

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,6 +1,6 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import UserProfilePage from 'src/pages/UserProfile/UserProfilePage';
-import MainPage from 'src/pages/Main/MainPage';
+import MainPage from 'src/pages/MainPage/MainPage';
 
 const AppRouter = () => {
   return (
